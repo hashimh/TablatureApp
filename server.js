00425ffdd61a447fd59decda62bd14643eb33cfe
Hashim Hussain <hashim_hussain98@hotmail.co.uk>
2018-11-06 15:08:04 +0000

    Server progress
    still doesnt work

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 8080
 
-app.get('/', (req, res) => res.send("Hello World!"));
 app.use('/', (req, res, next) => { console.log(new Date(), req.method, req.url); next(); });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
