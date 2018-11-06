71fcb81b5820f704c10eefcce1a94d0a7b1f8fec
Hashim Hussain <hashim_hussain98@hotmail.co.uk>
2018-11-06 14:32:48 +0000

    Server Development
    Minor changes to server

--- MAIN/Server/server.js
+++ MAIN/Server/server.js
@@ -2,9 +2,9 @@
 
 const express = require('express');
 const app = express();
-const path = require('path');
+const port = 8080
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+app.get('/', (req, res) => res.send("Hello World!"));
+app.use('/', (req, res, next) => { console.log(new Date(), req.method, req.url); next(); });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
