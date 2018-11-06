092ad045ab9cbd6a369333d670690f885f070ac7
Hashim Hussain <hashim_hussain98@hotmail.co.uk>
2018-11-06 17:04:29 +0000

    Basic Menu Page
    Created a basic menu page

---
+++ server/server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use('/', (req, res, next) => { console.log(new Date(), req.method, req.url); next(); });
+
+//app.get('/', function(req, res) {
+//  res.sendFile(path.join(__dirname + '/../webpages/login.html'));
+//});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
