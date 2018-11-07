e053443ff5f3c21d1152308c09260f202383fcfe
Hashim Hussain <hashim_hussain98@hotmail.co.uk>
2018-11-07 15:14:49 +0000

    misc
    no progress

--- README.md
+++
@@ -1,8 +0,0 @@
-# Dissertation-CodeFiles
-
-Packages:
-- express
-
-Google Auth Details:
-- Client ID: 566769051678-k1mpvtssd5jin7p6bekdtv7g1r23qav4.apps.googleusercontent.com
-- Client Secret: fDef9lsrc3Qt8w3cz5wpY-1k
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const app = express();
 const path = require('path');
 
 app.use('/', (req, res, next) => { console.log(new Date(), req.method, req.url); next(); });
+app.use('/', express.static('../webpages'));
+
 
 //app.get('/', function(req, res) {
 //  res.sendFile(path.join(__dirname + '/../webpages/login.html'));
