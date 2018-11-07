d0b233476acc882cea465ab25a1643670134ea20
Hashim Hussain <hashim_hussain98@hotmail.co.uk>
2018-11-07 18:27:25 +0000

    SERVER ATTEMPTS 2
    still no luck

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const express = require('express');
-const app = express();
-const path = require('path');
-const GoogleAuth = require('simple-google-openid');
+const express     = require('express');
+const app         = express();
+const path        = require('path');
+const GoogleAuth  = require('simple-google-openid');
 
 app.use('/', (req, res, next) => { console.log(new Date(), req.method, req.url); next(); });
-app.use('/', express.static('/../webpages'));
+// app.use('/', express.static('../webpages'));
 
-//app.get('/', function(req, res) {
-//  res.sendFile(path.join(__dirname + '/../webpages/login.html'));
-//});
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/../webpages/login.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
