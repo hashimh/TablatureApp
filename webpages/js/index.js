cefde55ba19809988cb98f0e3a5fb54f01d3f382
Hashim Hussain <hashim_hussain98@hotmail.co.uk>
2019-04-01 20:44:11 +0100

    Sign Out and Help Buttons
    Added Modal for help button, displays UNFINISHED information about the app.
    Sign out button works with confirmation from user.

--- webpages/js/index.js
+++ webpages/js/index.js
@@ -74,6 +74,26 @@ async function signOut() {
 
 
 // ----------------------------------------------------------------------------------------------- //
+// Function to sign out of the menu.html form ---------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------- //
+async function signOut2() {
+  if (confirm("All changes will be lost, continue with sign out?")) {
+    // Call server function 'logout'
+    let apiLink = '/api/logout';
+    await getPage(apiLink);
+
+    window.location.reload();
+
+    // Removes ID Token from local storage, ensures Google account logs out properly.
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("googleUser");
+  } else {
+    return;
+  }
+}
+
+
+// ----------------------------------------------------------------------------------------------- //
 // Function to go to main.html form -------------------------------------------------------------- //
 // ----------------------------------------------------------------------------------------------- //
 async function createTabBtn() {
@@ -99,6 +119,40 @@ async function populateMain() {
   fretBoard();
   chordFretboard();
 }
+
+
+
+// ----------------------------------------------------------------------------------------------- //
+// Code for the help modal popup ----------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------------------- //
+// Get the modal
+function helpBtn() {
+  let modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  let btn = document.getElementById("helpBtn");
+
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  modal.style.display = "block";
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+
+
 
 // ----------------------------------------------------------------------------------------------- //
 // ----------------------------------------------------------------------------------------------- //
