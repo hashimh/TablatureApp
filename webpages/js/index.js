0f91a9c9ae424cec2b4f1e5a5d903b1b56321a06
Hashim Hussain <up815459@myport.ac.uk>
2020-04-20 13:06:57 +0100

    updated grid layout for showing tablatures

--- webpages/js/index.js
+++ webpages/js/index.js
@@ -15,6 +15,19 @@ window.addEventListener("load", async (event) => {
   populateTable(tabInfo);
 });
 
+// We need to keep track of faded in elements so we can apply fade out later in CSS
+document.addEventListener("animationstart", function (e) {
+  if (e.animationName === "fade-in") {
+    e.target.classList.add("did-fade-in");
+  }
+});
+
+document.addEventListener("animationend", function (e) {
+  if (e.animationName === "fade-out") {
+    e.target.classList.remove("did-fade-in");
+  }
+});
+
 // ----------------------------------------------------------------------------------------------- //
 // Function to populate tab results table -------------------------------------------------------- //
 // ----------------------------------------------------------------------------------------------- //
